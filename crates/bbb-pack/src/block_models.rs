798887edd1fa4351e17af2c17eed2f2c68045ac6
Zhang Guyu <zhangguyu6@gmail.com>
2026-06-13 16:05:27 +0800

    Split pack block model types

--- crates/bbb-pack/src/block_models.rs
+++ crates/bbb-pack/src/block_models.rs
@@ -1,7 +1,6 @@
 use std::collections::{BTreeMap, HashMap};
 
 use anyhow::{Context, Result};
-use serde::{Deserialize, Serialize};
 
 use crate::PackRoots;
 
@@ -10,115 +9,16 @@ mod raw_model;
 mod resolver;
 mod rotation;
 mod shape;
+mod types;
 
 use blockstates::RawBlockstate;
 use raw_model::{RawBlockElement, RawBlockModel};
 use resolver::resolve_texture_alias;
 use rotation::{apply_variant_rotation, rotate_model_shape};
 use shape::{classify_model_shape, combine_model_shapes};
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Serialize, Deserialize)]
-pub enum BlockModelFace {
-    Down,
-    Up,
-    North,
-    South,
-    West,
-    East,
-}
-
-impl BlockModelFace {
-    pub const ALL: [Self; 6] = [
-        Self::Down,
-        Self::Up,
-        Self::North,
-        Self::South,
-        Self::West,
-        Self::East,
-    ];
-
-    pub fn index(self) -> usize {
-        match self {
-            Self::Down => 0,
-            Self::Up => 1,
-            Self::North => 2,
-            Self::South => 3,
-            Self::West => 4,
-            Self::East => 5,
-        }
-    }
-
-    fn name(self) -> &'static str {
-        match self {
-            Self::Down => "down",
-            Self::Up => "up",
-            Self::North => "north",
-            Self::South => "south",
-            Self::West => "west",
-            Self::East => "east",
-        }
-    }
-
-    fn from_name(name: &str) -> Option<Self> {
-        match name {
-            "down" => Some(Self::Down),
-            "up" => Some(Self::Up),
-            "north" => Some(Self::North),
-            "south" => Some(Self::South),
-            "west" => Some(Self::West),
-            "east" => Some(Self::East),
-            _ => None,
-        }
-    }
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
-pub struct BlockFaceTextures {
-    pub textures: [String; 6],
-    pub tint_indices: [Option<i32>; 6],
-}
-
-impl BlockFaceTextures {
-    pub fn get(&self, face: BlockModelFace) -> &str {
-        &self.textures[face.index()]
-    }
-
-    pub fn tint_index(&self, face: BlockModelFace) -> Option<i32> {
-        self.tint_indices[face.index()]
-    }
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
-pub enum BlockModelShape {
-    Cube,
-    Cross,
-    Box(BlockModelBox),
-    Boxes(Vec<BlockModelBox>),
-    Custom,
-}
-
-impl Default for BlockModelShape {
-    fn default() -> Self {
-        Self::Custom
-    }
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
-pub struct BlockRenderModel {
-    pub face_textures: BlockFaceTextures,
-    pub shape: BlockModelShape,
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
-pub struct BlockModelBox {
-    pub from: [u8; 3],
-    pub to: [u8; 3],
-    pub face_present: [bool; 6],
-    pub face_uvs: [[u8; 4]; 6],
-    pub face_cull: [bool; 6],
-    pub face_tint_indices: [Option<i32>; 6],
-    pub face_textures: [Option<String>; 6],
-}
+pub use types::{
+    BlockFaceTextures, BlockModelBox, BlockModelFace, BlockModelShape, BlockRenderModel,
+};
 
 #[derive(Debug, Clone)]
 pub struct BlockModelCatalog {
---
+++ crates/bbb-pack/src/block_models/types.rs
@@ -0,0 +1,104 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Serialize, Deserialize)]
+pub enum BlockModelFace {
+    Down,
+    Up,
+    North,
+    South,
+    West,
+    East,
+}
+
+impl BlockModelFace {
+    pub const ALL: [Self; 6] = [
+        Self::Down,
+        Self::Up,
+        Self::North,
+        Self::South,
+        Self::West,
+        Self::East,
+    ];
+
+    pub fn index(self) -> usize {
+        match self {
+            Self::Down => 0,
+            Self::Up => 1,
+            Self::North => 2,
+            Self::South => 3,
+            Self::West => 4,
+            Self::East => 5,
+        }
+    }
+
+    pub(super) fn name(self) -> &'static str {
+        match self {
+            Self::Down => "down",
+            Self::Up => "up",
+            Self::North => "north",
+            Self::South => "south",
+            Self::West => "west",
+            Self::East => "east",
+        }
+    }
+
+    pub(super) fn from_name(name: &str) -> Option<Self> {
+        match name {
+            "down" => Some(Self::Down),
+            "up" => Some(Self::Up),
+            "north" => Some(Self::North),
+            "south" => Some(Self::South),
+            "west" => Some(Self::West),
+            "east" => Some(Self::East),
+            _ => None,
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub struct BlockFaceTextures {
+    pub textures: [String; 6],
+    pub tint_indices: [Option<i32>; 6],
+}
+
+impl BlockFaceTextures {
+    pub fn get(&self, face: BlockModelFace) -> &str {
+        &self.textures[face.index()]
+    }
+
+    pub fn tint_index(&self, face: BlockModelFace) -> Option<i32> {
+        self.tint_indices[face.index()]
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub enum BlockModelShape {
+    Cube,
+    Cross,
+    Box(BlockModelBox),
+    Boxes(Vec<BlockModelBox>),
+    Custom,
+}
+
+impl Default for BlockModelShape {
+    fn default() -> Self {
+        Self::Custom
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub struct BlockRenderModel {
+    pub face_textures: BlockFaceTextures,
+    pub shape: BlockModelShape,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub struct BlockModelBox {
+    pub from: [u8; 3],
+    pub to: [u8; 3],
+    pub face_present: [bool; 6],
+    pub face_uvs: [[u8; 4]; 6],
+    pub face_cull: [bool; 6],
+    pub face_tint_indices: [Option<i32>; 6],
+    pub face_textures: [Option<String>; 6],
+}
