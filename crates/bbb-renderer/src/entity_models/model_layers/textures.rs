21f0e35ad294de2d2fdc4bf9bd7813203008918d
Zhang Guyu <zhangguyu6@gmail.com>
2026-06-21 08:39:57 +0800

    Split equine entity texture refs

--- crates/bbb-renderer/src/entity_models/model_layers/textures.rs
+++ crates/bbb-renderer/src/entity_models/model_layers/textures.rs
@@ -1,5 +1,9 @@
 use super::super::EntityModelTextureRef;
 
+mod equine;
+
+pub(in crate::entity_models) use equine::*;
+
 pub(in crate::entity_models) const PLAYER_WIDE_STEVE_TEXTURE_REF: EntityModelTextureRef =
     EntityModelTextureRef {
         path: "textures/entity/player/wide/steve.png",
@@ -696,66 +700,6 @@ pub(in crate::entity_models) const ENTITY_MODEL_TEXTURE_REFS: [EntityModelTextur
 pub fn entity_model_texture_refs() -> &'static [EntityModelTextureRef] {
     &ENTITY_MODEL_TEXTURE_REFS
 }
-
-pub(in crate::entity_models) const HORSE_WHITE_TEXTURE_REF: EntityModelTextureRef =
-    EntityModelTextureRef {
-        path: "textures/entity/horse/horse_white.png",
-        size: [64, 64],
-    };
-
-pub(in crate::entity_models) const HORSE_WHITE_BABY_TEXTURE_REF: EntityModelTextureRef =
-    EntityModelTextureRef {
-        path: "textures/entity/horse/horse_white_baby.png",
-        size: [64, 64],
-    };
-
-pub(in crate::entity_models) const DONKEY_TEXTURE_REF: EntityModelTextureRef =
-    EntityModelTextureRef {
-        path: "textures/entity/horse/donkey.png",
-        size: [64, 64],
-    };
-
-pub(in crate::entity_models) const DONKEY_BABY_TEXTURE_REF: EntityModelTextureRef =
-    EntityModelTextureRef {
-        path: "textures/entity/horse/donkey_baby.png",
-        size: [64, 64],
-    };
-
-pub(in crate::entity_models) const MULE_TEXTURE_REF: EntityModelTextureRef =
-    EntityModelTextureRef {
-        path: "textures/entity/horse/mule.png",
-        size: [64, 64],
-    };
-
-pub(in crate::entity_models) const MULE_BABY_TEXTURE_REF: EntityModelTextureRef =
-    EntityModelTextureRef {
-        path: "textures/entity/horse/mule_baby.png",
-        size: [64, 64],
-    };
-
-pub(in crate::entity_models) const SKELETON_HORSE_TEXTURE_REF: EntityModelTextureRef =
-    EntityModelTextureRef {
-        path: "textures/entity/horse/horse_skeleton.png",
-        size: [64, 64],
-    };
-
-pub(in crate::entity_models) const SKELETON_HORSE_BABY_TEXTURE_REF: EntityModelTextureRef =
-    EntityModelTextureRef {
-        path: "textures/entity/horse/horse_skeleton_baby.png",
-        size: [64, 64],
-    };
-
-pub(in crate::entity_models) const ZOMBIE_HORSE_TEXTURE_REF: EntityModelTextureRef =
-    EntityModelTextureRef {
-        path: "textures/entity/horse/horse_zombie.png",
-        size: [64, 64],
-    };
-
-pub(in crate::entity_models) const ZOMBIE_HORSE_BABY_TEXTURE_REF: EntityModelTextureRef =
-    EntityModelTextureRef {
-        path: "textures/entity/horse/horse_zombie_baby.png",
-        size: [64, 64],
-    };
 
 pub(in crate::entity_models) const CAMEL_TEXTURE_REF: EntityModelTextureRef =
     EntityModelTextureRef {
---
+++ crates/bbb-renderer/src/entity_models/model_layers/textures/equine.rs
@@ -0,0 +1,61 @@
+use super::super::super::EntityModelTextureRef;
+
+pub(in crate::entity_models) const HORSE_WHITE_TEXTURE_REF: EntityModelTextureRef =
+    EntityModelTextureRef {
+        path: "textures/entity/horse/horse_white.png",
+        size: [64, 64],
+    };
+
+pub(in crate::entity_models) const HORSE_WHITE_BABY_TEXTURE_REF: EntityModelTextureRef =
+    EntityModelTextureRef {
+        path: "textures/entity/horse/horse_white_baby.png",
+        size: [64, 64],
+    };
+
+pub(in crate::entity_models) const DONKEY_TEXTURE_REF: EntityModelTextureRef =
+    EntityModelTextureRef {
+        path: "textures/entity/horse/donkey.png",
+        size: [64, 64],
+    };
+
+pub(in crate::entity_models) const DONKEY_BABY_TEXTURE_REF: EntityModelTextureRef =
+    EntityModelTextureRef {
+        path: "textures/entity/horse/donkey_baby.png",
+        size: [64, 64],
+    };
+
+pub(in crate::entity_models) const MULE_TEXTURE_REF: EntityModelTextureRef =
+    EntityModelTextureRef {
+        path: "textures/entity/horse/mule.png",
+        size: [64, 64],
+    };
+
+pub(in crate::entity_models) const MULE_BABY_TEXTURE_REF: EntityModelTextureRef =
+    EntityModelTextureRef {
+        path: "textures/entity/horse/mule_baby.png",
+        size: [64, 64],
+    };
+
+pub(in crate::entity_models) const SKELETON_HORSE_TEXTURE_REF: EntityModelTextureRef =
+    EntityModelTextureRef {
+        path: "textures/entity/horse/horse_skeleton.png",
+        size: [64, 64],
+    };
+
+pub(in crate::entity_models) const SKELETON_HORSE_BABY_TEXTURE_REF: EntityModelTextureRef =
+    EntityModelTextureRef {
+        path: "textures/entity/horse/horse_skeleton_baby.png",
+        size: [64, 64],
+    };
+
+pub(in crate::entity_models) const ZOMBIE_HORSE_TEXTURE_REF: EntityModelTextureRef =
+    EntityModelTextureRef {
+        path: "textures/entity/horse/horse_zombie.png",
+        size: [64, 64],
+    };
+
+pub(in crate::entity_models) const ZOMBIE_HORSE_BABY_TEXTURE_REF: EntityModelTextureRef =
+    EntityModelTextureRef {
+        path: "textures/entity/horse/horse_zombie_baby.png",
+        size: [64, 64],
+    };
